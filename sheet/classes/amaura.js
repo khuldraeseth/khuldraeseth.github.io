6450ecf82b2fd7a4007b5f0868116cefa31a4db7
Brian Rago <28711596+khuldraeseth@users.noreply.github.com>
2019-09-23 20:54:51 -0400

    Add WIP Pokemon D&D stuff

---
+++ sheet/classes/amaura.js
@@ -0,0 +1,108 @@
+classes.amaura = 
+
+{
+    "base": "amaura",
+    
+    "evos": {
+        "basic": {
+            "level": 1,
+            "name": "Treecko",
+            "skills": 2,
+            "moves": 6
+        },
+
+        "stage1": {
+            "level": 8,
+            "name": "Aurorus",
+            "skills": 4,
+            "moves": 12
+        }
+    },
+
+    "types": [
+        "ice",
+        "rock"
+    ],
+
+    "races": [
+        "monster"
+    ],
+
+    "egg-groups": [
+        "dragon",
+        "monster"
+    ],
+
+    "saves": {
+        "con": 1,
+        "cha": 1,
+    },
+
+    "abilities": {
+        "refrigerate": 1,
+        "silver_dragon": 1,
+        "draconic_resilience": 1,
+        "metamagic": 3,
+        "mage hand": 4,
+        "elemental_affinity": 6,
+        "metametamagic": 10,
+        "dragon_wings": 14,
+        "metametametamagic": 17,
+        "aurorus_mastery": 20
+    },
+
+    "move_pool": {
+        "powder_snow": 1,
+        "thunder_wave": 1,
+        "growl": 1,
+        "dancing_lights": 1,
+        "ray_of_frost": 1,
+        "minor_illusion": 1,
+        "message": 1,
+        "magic_missile": 1,
+        "color_spray": 1,
+        "rock_throw": 1,
+        "detect_magic": 1,
+        "misty_step": 1,
+        "icy_wind": 3,
+        "take_down": 3,
+        "magic_armor": 3,
+        "aurora_beam": 3,
+        "mirror_image": 3,
+        "ancient_power": 5,
+        "sleet_storm": 5,
+        "gaseous_form": 5,
+        "tongues": 5,
+        "dispel_magic": 5,
+        "ice_storm": 5,
+        "avalanche": 7,
+        "nature_power": 7,
+        "polymorph": 7,
+        "invisibility": 9,
+        "cone_of_cold": 9,
+        "encore": 9,
+        "teleport": 9,
+        "light_screen": 9,
+        "prestidigitation_5PP": 9,
+        "ice_beam": 11,
+        "true_sight": 11,
+        "hyper_beam": 13,
+        "prismatic_spray": 13,
+        "scatter": 13,
+        "blizzard": 15,
+        "power_word_stun": 15,
+        "domination": 15,
+        "gate": 17,
+        "telekinesis": 17,
+        "draconic_aura": 17
+    },
+
+    "skill_pool": [
+        "arcana",
+        "deception",
+        "insight",
+        "intimidation",
+        "persuasion",
+        "religion"
+    ]
+}
---
+++ sheet/classes/budew.js
@@ -0,0 +1,85 @@
+classes.budew = 
+
+{
+    "evos": {
+        "basic": {
+            "level": 1,
+            "name": "Treecko",
+            "skills": 2,
+            "moves": 4
+        },
+
+        "stage1": {
+            "level": 3,
+            "name": "Grovyle",
+            "skills": 2,
+            "moves": 6
+        },
+
+        "stage2": {
+            "level": 7,
+            "name": "Sceptile",
+            "skills": 2,
+            "moves": 7
+        }
+    },
+
+    "types": [
+        "grass",
+        "poison"
+    ],
+
+    "races": [
+        "dragon",
+        "monster"
+    ],
+
+    "egg-groups": [
+        "dragon",
+        "grass",
+        "monster"
+    ],
+
+    "saves": {
+        "dex": 1,
+        "int": 1,
+        "wis": 15
+    },
+
+    "abilities": {
+        "overgrow": 1,
+        "unburden": 1,
+        "sneak_attack": 1,
+        "cunning_action": 2,
+        "uncanny_dodge": 5,
+        "evasion": 7,
+        "reliable_talent": 11,
+        "elusive": 18
+    },
+
+    "move_pool": {
+        "leer": 1,
+        "absorb": 1,
+        "pursuit": 3,
+        "screech": 3,
+        "leaf_blade": 7,
+        "agility": 7,
+        "detect": 9,
+        "synthesis": 9,
+        "plant_growth": 11,
+        "false_swipe": 12,
+        "leaf_storm": 13,
+        "tree_stride": 15
+    },
+
+    "skill_pool": [
+        "animal_handling",
+        "athletics",
+        "insight",
+        "investigation",
+        "nature",
+        "perception",
+        "stealth",
+        "survival"
+    ]
+}
